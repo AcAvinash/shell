07b02572b3694cbd5ffb3bab5a1a07616bc53172
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-10-03 23:23:52 +0530

    adding function script

---
+++ function.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+Red="\033[31m"
+Green="\033[32m"
+Reset="\033[0m"
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "You must be root to run this script."
+    exit 1
+fi
+
+# Function to check if a command exists
+Validate(){
+  if [ $1 -ne 0 ]; then
+    echo " ${Red}  $2 is failure ${Reset}"
+    exit 1
+  else
+    echo " ${Green}  $2 is success${Reset}"
+  fi
+}
+
+# Function to install a package using yum
+yum install mysql-server -y
+Validate $? "MySQL installation"
+
+yum install postfix -y
+Validate $? "Postfix installation"
