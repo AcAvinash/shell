4d0af892a39950ad2742d969b90ae1a8057743e7
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-10-03 22:33:15 +0530

    adding promt to read input from user

---
+++ prompt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Promt to read the input from user
+echo "Enter your name: "
+read USERNAME
+echo "Hello, $USERNAME! Welcome to the bash scripting world."
+echo "Enter your pasword:"
+read -s PASSWORD
+echo "Password for user $USERNAME is set."
+# Note: -s option is used to hide the input while typing the password
+# -p option is used to prompt the user with a message
